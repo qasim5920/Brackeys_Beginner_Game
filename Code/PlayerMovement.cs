4128fb3350c8c8baacaaed9f403ec4e8ff370930
Qasim Tariq <50355948+qasim5920@users.noreply.github.com>
2020-11-13 04:12:13 +0500

    Add files via upload

---
+++ Code/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float sideWaysForce = 500f;
+    public float forwardForce=2000f;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideWaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange );
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideWaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ Code/playerCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+       if(collisionInfo.collider.tag == "Obstacle") {
+            movement.enabled = false;
+        }
+    }
+}
