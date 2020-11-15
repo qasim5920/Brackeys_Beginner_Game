4fc4a44a776af12b23883c6bb61008bf7257644f
Qasim Tariq <50355948+qasim5920@users.noreply.github.com>
2020-11-15 19:20:47 +0500

    Add files via upload

--- Code/playerCollision.cs
+++ Code/playerCollision.cs
@@ -3,10 +3,13 @@
 public class playerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
+  
+
     private void OnCollisionEnter(Collision collisionInfo)
     {
        if(collisionInfo.collider.tag == "Obstacle") {
             movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
